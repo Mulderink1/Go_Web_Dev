9a3e5fa831d19391938657ce9de59805d0ec6ef9
Mike Mulderink <mulderink.code@gmail.com>
2020-05-14 23:19:43 -0500

    Minified File

--- Cookies/main.go
+++ Cookies/main.go
@@ -16,21 +16,17 @@ func main() {
 				Value: "0",
 			}
 		}
-
 		count, err := strconv.Atoi(cookie.Value)
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		count++
 		cookie.Value = strconv.Itoa(count)
 		http.SetCookie(w, cookie)
 		fmt.Fprint(w, fmt.Sprintf(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>Cookie Value: %d</strong></body></html>`, count))
 	})
-
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.NotFoundHandler()
 	})
-
 	http.ListenAndServe(":8080", nil)
 }
